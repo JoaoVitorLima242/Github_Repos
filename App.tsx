425e21bdfe63d31826865ca9fb363eb78e76ccec
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-30 21:02:32 -0300

    add styled components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function App() {
   return (
