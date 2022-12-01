c917acf9d6df3cce430808c748297e5bf6d632cf
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-30 23:22:17 -0300

    add loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter'
 
 import { RepoContextProvider } from './src/contexts/Repos'
 import Routes from './src/routes/index.routes'
 import { defaultTheme } from './src/styles/theme'
+import AppLoading from 'expo-app-loading'
 
 export default function App () {
+  const [fontsLoaded] = useFonts({
+    Roboto_500Medium,
+    Inter_400Regular,
+    Inter_700Bold
+  })
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
     <RepoContextProvider>
       <ThemeProvider theme={defaultTheme}>
